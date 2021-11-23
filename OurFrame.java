6e2c27ebe2ec78c7dbe7fe19812ebe7776bfa38d
Francisco <92608122+franserr33@users.noreply.github.com>
2021-11-22 20:35:59 -0800

    Update OurFrame.java

--- OurFrame.java
+++ OurFrame.java
@@ -1,14 +1,16 @@
 import javax.swing.JFrame;
-// inherit JFrame
 public class OurFrame extends JFrame{
-    //write Default constructor for class
+    /**
+     * Configure Frame to our needs, instantiate an OurPanel object and insert into frame
+     */
     OurFrame(){ 
-        OurPanel panelg = new OurPanel();// create instance of our OurPanel class
-        this.add(panelg); // insert panel into frame
+        // the timer class will continously create new instances of the panel class and pass references to the panelg var
+        OurPanel panelg = new OurPanel();
+        this.add(panelg); 
         this.setTitle("Francisco's Snake Game!!");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // let user exit when they press the X
-        this.setResizable(false); // don't let user  resize 
-        this.pack(); // the pack function will take our JFrame and fit it around the components 
+        this.setResizable(false); 
+        this.pack();// will take our frame and fit it around the components 
         this.setVisible(true); // frames are initially invisible, change it to visible
         this.setLocationRelativeTo(null); // game appears in the middle of the screen 
 
