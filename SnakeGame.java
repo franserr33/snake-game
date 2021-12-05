5eb9efa8b0db5774fb2a666c2d9622dd1e9e0f6d
Francisco <92608122+franserr33@users.noreply.github.com>
2021-12-04 23:46:24 -0800

    Update SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,8 +1,15 @@
+/**
+ * <h1> SnakeGame Demo </h1?
+ * To demo the snake game
+ * 
+ * <p>
+ * @author Francisco Javier Serrano Jr
+ * @version 1.1
+ * @since 11/2021
+ * </p>
+ */
 public class SnakeGame {
     public static void main(String[] args) throws Exception {
-        // instance of the Frame class with the panel I have placed inside
-        new OurFrame() ; 
-    
-    
+        new OurFrame() ;
     }
 }
