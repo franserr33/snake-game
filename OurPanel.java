5114ba07dbf89dea87f1fc28595023547b6fcb82
Francisco <92608122+franserr33@users.noreply.github.com>
2021-12-04 23:44:51 -0800

    Update OurPanel.java

--- OurPanel.java
+++ OurPanel.java
@@ -1,29 +1,69 @@
+/**
+ * <h1> Panel for the Snake Game </h1?
+ * Extends JPanel and implements ActionListener 
+ * Creates the Panel to place in OurFrame class.
+ * Has draw and move methods that are drawn every 60ms.
+ * Faster move rate to make it more challenging as I tweaked functionatily 
+ * so that when you collide with an edge you appear on the opposite side.
+ * Includes a GameOver scree.
+ * 
+ * 
+ * <p>
+ * @author Francisco Javier Serrano Jr
+ * @version 1.1
+ * @since 11/2021
+ * </p>
+ */
+
+
 import javax.swing.*;
+
 import java.awt.event.*; 
 import java.awt.*;
+
 import java.util.Random; 
-public class OurPanel  extends JPanel implements ActionListener {
-    /**create fields for screen dimensions, scaling factor for units, 
-    number of units, and the delay to pass onto timer instance*/ 
+public class OurPanel  extends JPanel implements ActionListener { 
+    /**
+     * Gives the width dimension 
+     */
     static final int SCREEN_W=600; 
+     /**
+     * Gives the height dimension 
+     */
     static final int SCREEN_H=600;
+     /**
+     * Gives the scaling factor for the units
+     */
     static final int UNIT_SIZE=25; 
+     /**
+     * Gives the amount of units 
+     */
     static final int GAME_UNITS= (SCREEN_H*SCREEN_W)/ UNIT_SIZE; 
-    static final int DELAY=80; 
-    // two arrays to hold coordinates of snake
+     /**
+     * Holds delay for timer 
+     */
+    static final int DELAY=60; 
+     /**
+     * Hold x-coordinates of snake 
+     */
     final int[] x= new int [GAME_UNITS] ; 
+    /**
+     * Hold y-coordinates of snake 
+     */
     final int[] y = new int [GAME_UNITS];
-    // snake starts with size 2, create variables for keeping count & apple coordinates, not running at start
+   // Start snake with length of 2 units
     int body=2; 
     int applesgotten, appleX, appleY; 
+    // start direction to the right
     char direction = 'R'; 
     boolean running = false; 
     Timer timer; 
     Random random; 
     /**
      * Default Constructor for my Panel. 
-     * Configure panel, allow it to be focused on, add action event listener for keys
-     * Event/Key listener is now an instance of MyKeyAdpater which extends KeyAdapter
+     * Configure panel
+     * Adds a key event listener which is an instance of a subclass of the KeyAdapter class
+     * Make call to method to use a timer instance 
      */
     OurPanel() { 
         random=new Random(); 
@@ -34,10 +74,9 @@ public class OurPanel  extends JPanel implements ActionListener {
         startGame(); 
     }
     /**
-     * Method call to start the game 
-     * Creates and gets apple coordinates randomly
-     * Methods depedning on a running=true are allowed to execute
-     * Timer instance acts on Panel constructor, an 80ms delay is passed on, and started
+     * Starts the process of drawing and updating location of snake and apple 
+     * Creates and gets apple coordinates at random 
+     * Timer instance acts on Panel constructor, a delay is passed on, and started
      */
     public void startGame() { 
         giveApple(); 
@@ -46,43 +85,45 @@ public class OurPanel  extends JPanel implements ActionListener {
         timer.start(); 
     } 
     /**
-     * Override paint component
-     * Calls the parent paintComponent method and calls draw method
+     * Override paint component, call parent paintComponent method
+     * and all your own draw method
+     * Called by repaint() method when an action event is performed 
      */
     public void paintComponent(Graphics g) { 
         super.paintComponent(g);
         draw(g); 
     }
+    /**
+     * All of my drawing will take place here except End Screen.
+     * Creates grid-lines in first for loop, draws apple, 
+     * colors the head of snake a different color, and colors rest of snake body.
+     * Lets player keep track of score
+     * Uses if/else to send to EndScreen with a GameOver method call
+     */
     public void draw(Graphics g) { 
         if(running) { 
-            // loop to create grid lines
+
             for (int i =0; i<SCREEN_H/UNIT_SIZE; i++ ) { 
                 g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_H);
                 g.drawLine(0, i* UNIT_SIZE, SCREEN_W, i* UNIT_SIZE); 
             }
-            // set to red then configure 
             g.setColor(Color.red); 
-            // fill with the coordinates of the apple then make sure the fill is as large as a single unit
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE); 
-            // loop to fill out the snake
             for(int i=0; i<body; i++) { 
-                if(i==0){ // for the head
+                if(i==0){ 
                     g.setColor(Color.green);
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE); 
                     }
-                    else { // rest of body give a different shade of green
+                    else { 
                         g.setColor(new Color (121,207,92) );
                         g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                     }
                 }
-            // visual for the score keeping
             g.setColor(Color.red); 
-            g.setFont(new Font ("Times New Roman",Font.BOLD,40)); // TNR, make it bold and 40 pixels
+            g.setFont(new Font ("Times New Roman",Font.BOLD,40)); 
             FontMetrics metrics = getFontMetrics(g.getFont());
-            // draw the string in the top middle 
             g.drawString("Score:"+applesgotten, (SCREEN_W-metrics.stringWidth("Score:"+applesgotten))/2, g.getFont().getSize());
         } else { 
-            // send to game over screen
             gameOver(g);
         }
     }
@@ -94,16 +135,18 @@ public class OurPanel  extends JPanel implements ActionListener {
         appleY=random.nextInt((int)(SCREEN_H/UNIT_SIZE))*UNIT_SIZE; 
     }
     /**
-     * To move the coordinates of snake around by loop and switch structure
+     * To move the coordinates of snake around.
+     * Reacts to change of direction from keyPressed method when called in actionPerformed method
+     * Movement is from tail-end pushing up toward the head of snake
+     * Switch decision structure to change direction of head
+     *
      */
-    
     public void move() { 
         for (int i=body;i>0;i--) { // push last element up the chain 
             x[i]=x[i-1]; 
             y[i]=y[i-1]; 
         }
-        // origin starts at upper-left corner
-        switch (direction) { // to move direction of the head, additional restrictions in subclass
+        switch (direction) { // recall that origin starts at upper-left corner
             case 'U': 
             y[0]=y[0]-UNIT_SIZE;
             break;
@@ -117,7 +160,6 @@ public class OurPanel  extends JPanel implements ActionListener {
             x[0]=x[0]+UNIT_SIZE;
             break; 
         }
-
     }
      /**
      * To increase counter, increase length and replace an apple when snake head reaches apple
@@ -128,33 +170,33 @@ public class OurPanel  extends JPanel implements ActionListener {
             applesgotten++; 
             giveApple();
         }
-        
     }
      /**
-     * Method to check if head hit panel's borders or itself
+     * To check if snake has hit itself
+     * Changed my version to let you go from one side of screen to the other
      */
     public void checkCollision () { 
     for (int i=body;i>0;i-- ){ 
         if((x[0]==x[i])&& (y[0]==y[i])) {
-                running=false; // flag as false to start the game over screen
+                running=false; 
         }
     }
-        if (x[0]<0) { // for left border
-            running=false; 
+        if (x[0]<0) { 
+            x[0]=SCREEN_W; 
         }
-        if (x[0]>SCREEN_W) { // for right border
-            running=false; 
+        if (x[0]>SCREEN_W) { 
+            x[0]=0; 
         }
-        if (y[0]<0) { // for top border
-            running=false; 
+        if (y[0]<0) { 
+            y[0]=SCREEN_H; 
+
         }
-        if (y[0]>SCREEN_H) { // for bottom border
-            running=false; 
+        if (y[0]>SCREEN_H) { 
+            y[0]=0; 
         }
         if (!running) { 
-            timer.stop(); // new panel instance is not created
+            timer.stop(); 
         }
-
     }
     /**
      * Creates and configures the GamevOver screen. 
@@ -168,12 +210,12 @@ public class OurPanel  extends JPanel implements ActionListener {
         g.setColor(Color.red); 
         g.setFont(new Font ("Times New Roman",Font.BOLD,80));
         FontMetrics metrics2 = getFontMetrics(g.getFont());
-        // puts the stuff in the very center of the screen 
         g.drawString("Game Over", (SCREEN_W-metrics2.stringWidth("Game Over"))/2, SCREEN_H/2);
     }
     /**
-     * Makes calls to these three methods continously as our grame is running 
-     * Once these changes are made,calls the repaint method
+     * When player touched keys and game is running then 
+     * move, checkApple and checkCollision methods are called
+     * Once these changes are made,calls the repaint method 
      */
     @Override
     public void actionPerformed(ActionEvent e) { 
@@ -191,7 +233,6 @@ public class OurPanel  extends JPanel implements ActionListener {
          */
         @Override 
         public void keyPressed(KeyEvent e) { 
-            // matches keycode to four discrete cases
             switch (e.getKeyCode()) { 
                 case KeyEvent.VK_LEFT: 
                     if (direction!='R') { 
